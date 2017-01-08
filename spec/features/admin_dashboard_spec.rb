251407d62243d6b9e9f4240fff67b608eb275cc4
konstantinsmirnov <k.smi@mail.ru>
2017-01-08 17:56:19 +0300

    Add dashboard

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -1,24 +1,27 @@
 require 'rails_helper'
 
 feature 'Dashboard' do
-  let(:admin) { FactoryGirl.create(:james_bond) }
 
-  before(:each) do
-    log_in_with(admin.email, 'password')
+  describe 'Visitor' do
+    scenario 'does not respond to dashboard_path if admin is not logged in' do
+      visit admin_dashboard_path
+
+      expect(current_path).to eq(root_path)
+    end
   end
 
-  scenario 'responds to dashboard_path' do
-    visit admin_dashboard_path
+  describe 'Admin' do
+    let(:admin) { FactoryGirl.create(:james_bond) }
 
-    expect(current_path).to eq(admin_dashboard_path)
-    expect(page).to have_text("Dashboard")
+    before(:each) do
+      log_in_with(admin.email, 'password')
+    end
+    scenario 'responds to dashboard_path' do
+      visit admin_dashboard_path
+
+      expect(current_path).to eq(admin_dashboard_path)
+      expect(page).to have_text("Dashboard")
+    end
   end
 
-  scenario 'does not respond to dashboard_path if admin is not logged in' do
-    click_link 'Log out'
-
-    visit admin_dashboard_path
-
-    expect(current_path).to eq(root_path)
-  end
 end
---
+++ spec/features/navbar_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'Navbar' do
+  let(:admin) { FactoryGirl.create(:james_bond) }
+
+  describe 'Visitor' do
+    scenario 'does not see a link on dashboard' do
+      visit root_path
+
+      expect(page).not_to have_selector("a", text: 'Admin')
+    end
+  end
+
+  describe 'Logged in admin' do
+    scenario 'can see a link on dashboard' do
+      log_in_with(admin.email, 'password')
+
+      click_link 'Admin'
+
+      expect(current_path).to eq(admin_dashboard_path)
+      expect(page).to have_text("Dashboard")
+    end
+  end
+
+end
