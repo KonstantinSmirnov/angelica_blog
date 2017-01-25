f791bc99be64bc8023d72550b3ca278f3af0b340
konstantinsmirnov <k.smi@mail.ru>
2017-01-25 23:28:17 +0300

    Update server IP

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 
-server '85.143.215.3', user: 'deployer', roles: [:web, :app, :db], primary: true
+server '89.223.30.90', user: 'deployer', roles: [:web, :app, :db], primary: true
 
 set :application, 'angelica-blog'
 set :repo_url, 'git@github.com:KonstantinSmirnov/angelica_blog.git'
